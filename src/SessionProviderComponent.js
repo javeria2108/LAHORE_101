84761d5ae1c69d299e6a193666c5acbc70bd3a96
javeria2108 <javeriazh@gmail.com>
2024-03-28 00:17:33 +0500

    env update

--- src/SessionProviderComponent.js
+++ src/SessionProviderComponent.js
@@ -4,7 +4,7 @@ import {SessionProvider as Provider } from 'next-auth/react';
 
 const SessionProvider = ({ children, session }) => {
   return (
-    <Provider session={session}>
+    <Provider session={session} basePath="/api/auth">
       {children}
     </Provider>
   );
